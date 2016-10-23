dbafed1c8570705dea312bfa62f6a7762256df09
Sergey <rabota2087@yandex.ru>
2016-10-24 00:40:07 +0300

    ispravlenie

--- arrays_loops_tasks/16.php
+++ arrays_loops_tasks/16.php
@@ -1,10 +1,9 @@
 <?php
 $arr = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
-foreach ($arr as $key => $value) {
-    if ($key == 2 || $key == 5) {
-        echo $value;
-    } else {
-        echo $value, "</br>";
+foreach ($arr as $value) {
+    echo $value;
+    if (($value % 3) == 0) {
+        echo "</br>";
     }
 }
--- php_basics_tasks/19.php
+++ php_basics_tasks/19.php
@@ -1,10 +1,5 @@
 <?php
 $a = '78';
 $b = 78;
-$c = NULL;
-if ($a == $b) {
-    $c = 'равны';
-}else {
-    $c = 'не равны';
-}
-var_dump($c);
+$c = false;
+var_dump($a == $b);
